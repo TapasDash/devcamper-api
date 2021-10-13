268bacaec8e239016dcec1ad877963bf8d1e0f66
Tapas Dash <tapas@theflak.in>
2021-10-13 16:23:37 +0530

    Initial setup

---
+++ server.js
@@ -0,0 +1,14 @@
+import express from "express";
+import dotenv from "dotenv";
+
+//load config vars
+dotenv.config({ path: "./config/config.env" });
+
+const app = express();
+
+const { PORT, NODE_ENV } = process.env;
+
+app.listen(
+  PORT,
+  console.log(`App is running on ${NODE_ENV} listening on the ${PORT}`)
+);
